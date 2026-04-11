d0799e879f2b1d73cd734206ab5bd26311062938
hopie <vuthihop06@gmail.com>
2026-04-11 22:41:27 +0700

    Delete some files

--- env.py
+++ env.py
@@ -3,6 +3,20 @@ import numpy as np
 import gymnasium as gym
 
 class SnakeBoardEnv(gym.Env):
-    def __init__(self, state_size, snake):
+    def __init__(self, box_dimension, snake):
+        self.box_dimension = box_dimension
+        self.set_observation_space(box_dimension)  #create_observation_space (including position of snake, apple)
+        self.snake = snake
+
+        # Initialize positions - reset()
+
+        # Define what the agent can observe
+    def
+    def step(self, action):
         pass
+    def reset(self):
+    def random_apple(self):
+        # No collide with Snake
+
+
 
